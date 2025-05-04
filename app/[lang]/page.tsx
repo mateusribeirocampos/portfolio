ad32eca1161ebefd9deea2bc1c2e2c1a2ea6ac5d
mateusribeirocampos <mateusrcampos@gmail.com>
2025-05-04 07:11:35 -0300

    feat: Decoder text implemented

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -4,9 +4,15 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ArrowRight, Code, Leaf, Smartphone } from "lucide-react";
 import { useTranslation } from "react-i18next";
+import { DecoderText } from '../../components/decoderText/decoderText';
+import { useInView } from 'react-intersection-observer';
 
 export default function Home() {
   const { t } = useTranslation('home');
+  const title = t('home.title');
+  const subtitle = t('home.subtitle');
+
+  const { ref: subtitleRef, inView: subtitleInView } = useInView({ triggerOnce: true, threshold: 0.3 });
 
   return (
     <>
@@ -14,7 +20,7 @@ export default function Home() {
         <section className="container flex flex-col items-center justify-center gap-4 py-24 md:py-72">
           <div className="flex flex-col items-center gap-4 text-center">
             <h1 className="text-3xl font-bold sm:text-4xl md:text-5xl lg:text-6xl">
-              {t('home.title')}
+              <DecoderText text={title} delay={600} />
             </h1>
             <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
               {t('home.pTitle')}
@@ -40,8 +46,8 @@ export default function Home() {
           <div className="container px-4">
             <div className="text-center mb-20 relative">
               <div className="items-center rounded-md py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-foreground mb-4">
-                <h2 className="inline-flex text-4xl md:text-5xl font-bold mb-6">
-                  {t('home.subtitle')}
+                <h2 ref={subtitleRef} className="inline-flex text-4xl md:text-5xl font-bold mb-6">
+                  <DecoderText text={subtitle} start={subtitleInView} delay={500} />
                 </h2>
                 <p className="text-lg text-muted-foreground max-w-2xl mx-auto">
                   {t('home.pSubtitle')}
